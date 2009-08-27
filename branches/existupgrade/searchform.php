857fbcf5832ed50bf63aa5e08751e0fba2e36c00
ahickco <ahickco@648cfba0-c786-42fc-82f8-821b261732f5>
2009-08-27 16:20:18 +0000

    Preparing for exist 1.2.6 upgrade to production.

---
+++ branches/existupgrade/searchform.php
@@ -0,0 +1,14 @@
+<?php
+include("config.php");
+include("common_functions.php");
+
+html_head("Search", "web/css/search.css");
+
+include("web/xml/browse-head.xml");
+include("searchoptions.php");
+include("web/xml/footer.xml");
+
+?>
+
+</body>
+</html>
